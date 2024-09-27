bca7daf33682acdfafe4486dfc1a5fadc780b2f8
Jeremy Karlsson <karlsson@jeremy.se>
2024-09-27 15:03:16 +0200

    Update build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
 }
 
 group = "se.jeremy"
@@ -13,4 +14,18 @@ repositories {
 
 dependencies {
     implementation("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        xml.required.set(true)
+        html.required.set(false)
+    }
 }
